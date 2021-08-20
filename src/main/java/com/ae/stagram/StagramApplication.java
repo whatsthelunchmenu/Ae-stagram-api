1a6e44b5323106d759fa9040ba277dce766c1894
phantom08266 <phantom08266@gmail.com>
2021-08-20 12:12:40 +0900

    [Add] 프로젝트 생성

---
+++ src/main/java/com/ae/stagram/StagramApplication.java
@@ -0,0 +1,13 @@
+package com.ae.stagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StagramApplication.class, args);
+    }
+
+}
