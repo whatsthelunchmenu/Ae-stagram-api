d4025e950f12caee2c7339bd9f690bfafd1b9c56
phantom08266 <phantom08266@gmail.com>
2021-08-26 18:00:50 +0900

    [Fix] firebase sdk 키 read 처리
    배포환경에서는 resource에 위치한 파일을 읽을때 문제가 있어 ClassPathResource를 통해 getInputStream 사용하여 리소스의 위치에 상관없이 읽을 수 있도록 처리

--- src/main/java/com/ae/stagram/config/FirebaseConfig.java
+++ src/main/java/com/ae/stagram/config/FirebaseConfig.java
@@ -4,13 +4,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import java.io.FileInputStream;
 import java.io.IOException;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * PostConstruct애노테이션은 객체가 생성된 후 별도의 초기화가 필요할 사용하는 애노테이션 Primary애노테이션을 사용하여 가장 먼저 초기화가 되도록 설정
@@ -26,12 +26,10 @@ public class FirebaseConfig {
     @Primary
     @PostConstruct
     public void initailize() throws IOException {
-//        String path = new ClassPathResource(firebaseAccessKey).getURL().toString();
-        FileInputStream serviceAccount = new FileInputStream(firebaseAdminSdkPath);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
             .setCredentials(GoogleCredentials
-                .fromStream(serviceAccount))
+                .fromStream(new ClassPathResource(firebaseAdminSdkPath).getInputStream()))
             .build();
 
         firebaseApp = FirebaseApp.initializeApp(options);
