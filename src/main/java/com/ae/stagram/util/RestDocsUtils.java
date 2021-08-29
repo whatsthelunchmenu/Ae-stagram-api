bc3f03c78cf525f8243fb0e26870f030aa2658fe
Lee June Hee <39672033+phantom08266@users.noreply.github.com>
2021-08-30 02:09:13 +0900

    Spring Rest Docs 반영 (#10)
    * [Add] rest docs 추가
    
    - rest docs 의존성 추가
    - rest docs 설정
    - docs 파일 추가
    https://github.com/whatsthelunchmenu/Ae-stagram-api/issues/9
    
    * [Fix] 인터셉터 단위테스트 무시처리
    
    현재 로그인 처리는 Firebase를 header를 통해 받아서 처리하고 있는데 단위테스트 특성 상 Firebase에서 생성해주는 tocken을 매번 발금해줄 수 없어 인터셉터가 무시되어 처리될 수 있도록 테스트 수정
    
    https://github.com/whatsthelunchmenu/Ae-stagram-api/issues/9
    
    * [Fix] Jenkins Build시 테스트케이스 수행
    
    - 빌드 시 junit test  동작하도록 CI 스크립트 수정 (build.gradle에 test 를 수행해야 asciidoc 폴더를 생성하기 때문)
    - Rest docs문서 접근 시 토큰없이 접근할 수 있도록 interceptor 체크 경로 수정
    
    * [Fix] CD 스크립트 수정
    
    프로젝트 빌드 시 asciidocs를 만들기 위해 스크립트 수정
    
    * [Fix] Spring Reset docs Resouce 파일 위치 수정
    
    정적리소스파일은 src/main/resources에서 관리함에따라 빌드 시 rest docs파일이 생성되도록 수정

---
+++ src/main/java/com/ae/stagram/util/RestDocsUtils.java
@@ -0,0 +1,26 @@
+package com.ae.stagram.util;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+public interface RestDocsUtils {
+
+    static OperationRequestPreprocessor getDocumentRequest(){
+        return preprocessRequest(
+            modifyUris()
+            .scheme("http")
+            .host("localhost")
+            .port(8080),
+            prettyPrint()
+        );
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse(){
+        return preprocessResponse(prettyPrint());
+    }
+}
